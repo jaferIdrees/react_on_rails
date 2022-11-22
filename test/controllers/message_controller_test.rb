fbffe0ac3d027144fda683d4ec932dd37b22502f
jaferidrees <jaferll@gmail.com>
2022-11-22 04:19:04 +0100

    Setup linters

--- test/controllers/message_controller_test.rb
+++ test/controllers/message_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class MessageControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test 'should get show' do
     get message_show_url
     assert_response :success
   end
